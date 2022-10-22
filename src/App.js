c6526146c6a04073b8ddc545c4b2fbadfa7f4ea2
fredkamm <fred.kamm95@gmail.com>
2022-10-22 14:33:10 -0400

    tailwind install, login page initial setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import * as ROUTES from './routes/routes';
+
+const Login = lazy(() => import('./pages/login'));
 
 function App() {
-  return <p>Hello you</p>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading... </p>}>
+        <Routes>
+          <Route path={ROUTES.LOGIN} element={<Login />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/login.js
@@ -0,0 +1,29 @@
+import { useState, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import FirebaseContext from '../context/firebase';
+
+export default function Login() {
+  const navigate = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+
+  //   Allowing the user to login
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Error if there is an error in the login
+  const [error, setError] = useState('');
+  const isInvalid = password === '' || emailAddress === '';
+
+  //   handles a successful login
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = 'Login - Instaclone';
+  }, []);
+
+  return (
+    <div>
+      <p>Login Page</p>
+    </div>
+  );
+}
