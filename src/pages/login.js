8c39d0cc83ca9faa62180e7ab90feb2449de07f9
fredkamm <fred.kamm95@gmail.com>
2022-10-23 10:12:46 -0400

    update

--- src/pages/login.js
+++ src/pages/login.js
@@ -25,16 +25,12 @@ export default function Login() {
   return (
     <div className="container flex ms-auto max-w-screen items-center h-screen">
       <div className="flex w-3/5">
-        <img
-          src="/images/iphone-with-profile.jpg"
-          alt="iPhone with Instagram app"
-          className="max-w-full"
-        />
+        <img src="/images/coverPhoto.png" alt="iPhone with Instagram app" className="max-w-full" />
       </div>
       <div className="flex flex-col w-2/5">
         <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
           <h1 className="flex justify-center w-full">
-            <img src="/images/logo.png" alt="instaclone" className="mt-2 w-6/12 mb-4" />
+            <img src="/images/instaclone.png" alt="instaclone" className="mt-2 w-6/12 mb-4" />
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
